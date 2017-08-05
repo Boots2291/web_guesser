d0550aa78c579e27fbf9df6ed959fcc8a1eb6c18
Boots2291 <buteramv@gmail.com>
2017-08-05 13:31:31 -0600

    Add changing background color

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,7 +5,9 @@ require 'sinatra/reloader'
 set :random_number, rand(101)
 
 def check_guess(guess)
-  if guess > settings.random_number && guess - settings.random_number > 5
+  if guess == 0
+    ""
+  elsif guess > settings.random_number && guess - settings.random_number > 5
     "Way to high!"
   elsif guess > settings.random_number
     "Too high!"
@@ -18,11 +20,30 @@ def check_guess(guess)
   end
 end
 
+def background(message)
+  case message
+  when "Way to high!"
+    "#ff2828"
+  when "Way to low!"
+    "#ff2828"
+  when "Too high!"
+    "#ff7777"
+  when "Too low!"
+    "#ff7777"
+  when "You got it right!"
+    "#48ff42"
+  else
+    "white"
+  end
+end
+
 get '/' do
   guess = params["guess"].to_i
   message = check_guess(guess)
+  background = background(message)
   erb :index, :locals => {:random_number => settings.random_number,
-                          :message => message}
+                          :message => message,
+                          :background => background}
 end
 
 
